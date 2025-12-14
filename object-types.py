98e5bb9ff31eb78c45fb9bfb87f8342d52962ad3
Aoife Bee <aoifebeeof@gmail.com>
2025-12-14 11:44:49 -0500

    added gameobject class and object types rectangle and circle

---
+++ object-types.py
@@ -0,0 +1,32 @@
+import pygame
+
+# Base Class for all game objects
+class GameObject(pygame.sprite.Sprite):
+    def __init__ (self, x, y):
+        self.x = x
+        self.y = y
+
+    def update(self, dt):
+        pass
+
+    def draw(self, screen):
+        pass
+
+    def collides_with(self, other):
+        pass
+
+class Rectangle(GameObject):
+    def __init__ (self, x, y, width, height):
+        super().__init__(x, y)
+        self.width = width
+        self.height = height
+        self.rect = pygame.Rect(x, y, width, height)
+    
+    def update_rect(self):
+        self.rect.width = self.width
+        self.rect.height = self.height
+
+class Circle(GameObject):
+    def __init__ (self, x, y, radius):
+        super().__init__(x, y)
+        self.radius = radius
